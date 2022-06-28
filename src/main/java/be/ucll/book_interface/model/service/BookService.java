6f5fbfdf2828612c45a2c99d7eaeb0a94b49591c
greetjeJongen <greetje.jongen@ucll.be>
2022-06-28 15:17:39 +0200

    service: initial commit && test

--- src/main/java/be/ucll/book_interface/model/service/BookService.java
+++ src/main/java/be/ucll/book_interface/model/service/BookService.java
@@ -12,4 +12,81 @@ public class BookService {
         return books;
     }
 
+    public double getTotalValueOfCollection() {
+        double result = 0;
+        for (Book book : books) {
+            result += book.getPrice();
+        }
+        return result;
+    }
+
+    public Book getBookWithTitle(String title) {
+        for (Book book : getBooks()) {
+            if (book.getTitle().equals(title))
+                return book;
+        }
+        return null;
+    }
+
+    public Book getMostExpensiveBook() {
+        if (this.getBooks().size() == 0)
+            return null;
+        Book result = getBooks().get(0);
+        for (Book book : books) {
+            if (book.getPrice() > result.getPrice())
+                result = book;
+        }
+        return result;
+    }
+
+    public List<Book> getBooksWithPriceMoreThen(double price) {
+        ArrayList<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getPrice() > price)
+                result.add(book);
+        }
+        return result;
+    }
+
+    public List<Book> getBooksInColor() {
+        ArrayList<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.isInColor())
+                result.add(book);
+        }
+        return result;
+    }
+
+    public List<Book> getBooksWithClassification(char c) {
+        ArrayList<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getClassification() == c)
+                result.add(book);
+        }
+        return result;
+    }
+
+    public boolean addBook(Book book) {
+        if (book == null)
+            return false;
+        if (getBookWithTitle(book.getTitle()) != null)
+            return false;
+        this.books.add(book);
+        return true;
+    }
+
+    public Book removeBook(Book book) {
+        if (books.remove(book))
+            return book;
+        return null;
+    }
+
+    public Book removeBook(String title) {
+        Book book = this.getBookWithTitle(title);
+        if (book != null) {
+            books.remove(book);
+            return book;
+        }
+        return null;
+    }
 }
